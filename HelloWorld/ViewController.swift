c68da487d3a38b0a6728c513d04038888e6e05d7
login2030 <login2030@gmail.com>
2022-10-23 11:34:33 +0500

    feat: greeting message show/hide

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel:UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        greetingLabel.isHidden = true
+        
+        greetingButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func greetingButtonTapped() {
+        greetingLabel.isHidden.toggle()
+        
+        let buttonText = greetingLabel.isHidden ? "Show Geeting" : "Hide Greeting"
+        greetingButton.setTitle(buttonText, for: .normal)
+    }
 }
 
